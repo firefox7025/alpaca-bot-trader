b159d262e342fe6d828537335dd31790b1def1c1
Alexander Montgomery <evaderxander@gmail.com>
2019-08-15 01:41:34 -0400

    initial work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate iron;
+extern crate time;
+
+use iron::prelude::*;
+
+mod controller;
+
+use crate::controller::ResponseTime;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut chain = Chain::new(controller::hello_world);
+    chain.link_before(ResponseTime);
+    chain.link_after(ResponseTime);
+    Iron::new(chain).http("localhost:3000").unwrap();
 }
